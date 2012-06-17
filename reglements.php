e3a23a2c0a46ff07032f1945855b2a5f0cc8f7c1
Arthur Puyou <arthur@puyou.fr>
2012-06-17 23:22:15 +0200

    Correction de messages d'erreur sql

--- reglements.php
+++ reglements.php
@@ -78,18 +78,19 @@ else {
 			$vSql ="SELECT * FROM park_reglement;";
 			$vQuery=pg_query($vConn, $vSql);
 			while ($park = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
-				$abb_id = $park['abonnement'];
-				$vSqlAbonne = "SELECT park_abonnement.id, park_personne.nom, park_personne.prenom,park_abonnement.type FROM park_abonnement INNER JOIN park_personne ON park_abonnement.abonne = park_personne.id WHERE park_abonnement.id = $abb_id;";
-				$vQueryAbonne=pg_query($vConn, $vSqlAbonne);
-				$abonne=pg_fetch_array($vQueryAbonne, null, PGSQL_ASSOC);
-				$tick_id = $park['ticket'];
-				$vSqlTick = "SELECT park_ticket.id, park_parking.nom FROM park_ticket INNER JOIN park_parking ON park_ticket.parking = park_parking.id WHERE park_ticket.id = $tick_id;";
-				$vQueryTick=pg_query($vConn, $vSqlTick);
-				$tick=pg_fetch_array($vQueryTick, null, PGSQL_ASSOC);
 				if (strcmp($park['type'], 'abb') == 0):
 					$type = 'Abonnement '.$abonne['type'];
-				elseif (strcmp($park['type'], 'ticket') == 0):
+                                        $abb_id = $park['abonnement'];
+                                        $vSqlAbonne = "SELECT park_abonnement.id, park_personne.nom, park_personne.prenom,park_abonnement.type FROM park_abonnement INNER JOIN park_personne ON park_abonnement.abonne = park_personne.id WHERE park_abonnement.id = $abb_id;"; 
+                                        $vQueryAbonne=pg_query($vConn, $vSqlAbonne);
+                                        $abonne=pg_fetch_array($vQueryAbonne, null, PGSQL_ASSOC);
+                                elseif (strcmp($park['type'], 'ticket') == 0):
 					$type = 'Ticket';
+               				$tick_id = $park['ticket'];
+                                        $vSqlTick = "SELECT park_ticket.id, park_parking.nom FROM park_ticket INNER JOIN park_parking ON park_ticket.parking = park_parking.id WHERE park_ticket.id = $tick_id;";
+                                        $vQueryTick=pg_query($vConn, $vSqlTick);
+                                        $tick=pg_fetch_array($vQueryTick, null, PGSQL_ASSOC);
+
 				endif;
 				echo '
 				<tr>
